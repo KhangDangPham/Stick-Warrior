a7b47095d3013ff1cc6274799100b8f836b0c5af
KhangDangPham <phamdkhang@outlook.com>
2024-10-29 00:11:49 -0500

    Updated Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speedForce = 5;
+    public float speedForce = 5.0f;
     public float jumpForce = 10f;
-    public Rigidbody2D rb;
+    public Rigidbody2D rigidBody;
     public Animator anim;
+    bool onLand;
     float movementSpeed;
-
-    bool onLand = true;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //rigidBody = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -43,34 +40,32 @@ public class PlayerController : MonoBehaviour
 
         else
         {
-            anim.SetBool("Jumping", false);
             anim.SetBool("Running", false);
         }
 
         // Jumping
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && onLand)
         {
-            anim.SetBool("Running", false);
-            if (onLand)
-            {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-                anim.SetBool("Jumping", true);
-            } else
-            {
-                anim.SetBool("Jumping", false);
-            }
+            PlayerJump();
         }
 
-        rb.linearVelocity = new Vector2(movementSpeed, rb.linearVelocity.y);
+        //anim.SetBool("Running", hDirection != 0);
+        anim.SetBool("Onland", onLand);
+        rigidBody.linearVelocity = new Vector2(movementSpeed, rigidBody.linearVelocity.y);
     }
 
-    void OnTriggerEnter2D()
+    void PlayerJump()
     {
-        onLand = true;
-    }
-
-    void OnTriggerExit2D()
-    {
+        rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpForce);
+        anim.SetTrigger("Jumping");
         onLand = false;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onLand = true;
+        }
     }
 }
