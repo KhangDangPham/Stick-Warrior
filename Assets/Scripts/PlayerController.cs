5ccdb6484f2fb4d87b34615d15177b45639d2727
KhangDangPham <phamdkhang@outlook.com>
2024-11-08 18:49:43 -0600

    Updated Animations And Created Second Build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,16 +10,21 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public Rigidbody2D rigidBody;
     public Animator anim;
-    bool onLand;
-    float movementSpeed;
+    public bool onLand;
+    public bool isMoving;
+    public bool isAttacking;
+    public bool isJumping;
+    public float movementSpeed;
     private Vector3 checkpoint;
     public GameObject fallDetector;
+    private IEnumerator attackTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        //rigidBody = GetComponent<Rigidbody2D>();
-        //anim = GetComponent<Animator>();
+        isMoving = true;
+        isAttacking = false;
+        isJumping = false;
         checkpoint = transform.position;
     }
 
@@ -27,15 +32,16 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float hDirection = Input.GetAxis("Horizontal");
+        isMoving = hDirection != 0;
 
-        // Right movement
-        if (hDirection > 0)
+        // Moving
+        if (hDirection > 0 && isMoving == true && isAttacking != true)
         {
             PlayerMove(speedForce, 1);
         }
 
         // Left movement
-        else if (hDirection < 0)
+        else if (hDirection < 0 && isMoving == true && isAttacking != true)
         {
             PlayerMove(-speedForce, -1);
         }
@@ -45,7 +51,7 @@ public class PlayerController : MonoBehaviour
         }
 
         // Jumping
-        if (Input.GetButtonDown("Jump") && onLand)
+        if (Input.GetButtonDown("Jump") && onLand == true)
         {
             PlayerJump();
         }
@@ -53,7 +59,8 @@ public class PlayerController : MonoBehaviour
         // Attacking
         if (Input.GetMouseButtonDown(0))
         {
-            PlayerAttack();
+            StartCoroutine(PlayerAttack());
+            isMoving = false;
         }
 
         // Falling
@@ -63,24 +70,18 @@ public class PlayerController : MonoBehaviour
 
         fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
     }
-
     void PlayerMove(float speed, int direction)
     {
         Vector2 movementVector = new Vector2(direction, Input.GetAxis("Vertical"));
         movementSpeed = speed;
         transform.localScale = new Vector2(direction, 1);
         anim.SetBool("Running", true);
-        //rigidBody.linearVelocity = new Vector2(movementSpeed * direction * Time.fixedDeltaTime, rigidBody.linearVelocity.y);
 
         if (direction > 0)
         {
-            //rigidBody.MovePosition(transform.position + movementVector * Time.fixedDeltaTime * movementSpeed);
-            //rigidBody.MovePosition(transform.position + Vector3.right * movementSpeed * Time.fixedDeltaTime);
             transform.Translate(movementVector * movementSpeed * Time.deltaTime * direction);
-        } else
+        } else if (direction < 0)
         {
-            //rigidBody.MovePosition(transform.position - movementVector * Time.fixedDeltaTime * movementSpeed);
-            //rigidBody.MovePosition(transform.position - Vector3.left * movementSpeed * Time.fixedDeltaTime);
             transform.Translate(movementVector * movementSpeed * Time.deltaTime * direction);
         }
     }
@@ -88,17 +89,13 @@ public class PlayerController : MonoBehaviour
     {
         movementSpeed = 0;
         anim.SetBool("Running", false);
-        //rigidBody.linearVelocity = new Vector2(movementSpeed, rigidBody.linearVelocity.y);
     }
-
     void PlayerJump()
     {
-        //rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpForce);
         rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         anim.SetTrigger("Jumping");
         onLand = false;
     }
-
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
@@ -111,14 +108,18 @@ public class PlayerController : MonoBehaviour
             transform.position = checkpoint;
         }
     }
-
     void PlayerFall()
     {
         anim.SetFloat("FallingBound", rigidBody.linearVelocity.y);
     }
-
-    void PlayerAttack()
+    IEnumerator PlayerAttack()
     {
+        anim.SetBool("Running", false);
         anim.SetTrigger("Attacking");
+        isMoving = false;
+        isAttacking = true;
+        yield return new WaitForSeconds(0.5f);
+        isAttacking = false;
+        isMoving = true;
     }
 }
